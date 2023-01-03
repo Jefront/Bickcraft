35b7d047847fe32075d89ef3bc56e6acd7518dbd
Jefferson Costa <jeffersoncostadacruz8@gmail.com>
2023-01-03 11:47:29 -0300

    03-01-2022 corringindo bugs e adicionando JS

---
+++ js/sricpt.js
@@ -0,0 +1,44 @@
+//Ativar menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add("ativo");
+    }
+}
+links.forEach(ativarLink);
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+}
+parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
