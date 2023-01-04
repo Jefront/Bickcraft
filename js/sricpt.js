aab4f1a74be0d78b6fa368df97a23e6c19524867
Jefferson Costa <jeffersoncostadacruz8@gmail.com>
2023-01-04 11:22:38 -0300

    04-01-2022 Plugin e Otimização do css

--- js/sricpt.js
+++ js/sricpt.js
@@ -42,3 +42,25 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria img
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+galeria.forEach(eventosGaleria);
+
+//Ativando plugin
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
